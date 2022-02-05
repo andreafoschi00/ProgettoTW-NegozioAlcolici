733fe26b4704504d4464157e785eb668bcbebdbe
Andrea Foschi <andrea.foschi13@studio.unibo.it>
2022-02-05 17:36:21 +0100

    add sql file for db creation

---
+++ db/negozio_alcolici_db.sql
@@ -0,0 +1,251 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Creato il: Feb 05, 2022 alle 17:34
+-- Versione del server: 10.4.21-MariaDB
+-- Versione PHP: 8.0.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `negozio_alcolici_tw`
+--
+CREATE DATABASE IF NOT EXISTS `negozio_alcolici_tw` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `negozio_alcolici_tw`;
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `categoria`
+--
+
+CREATE TABLE `categoria` (
+  `ID` int(11) NOT NULL,
+  `nome` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `cliente`
+--
+
+CREATE TABLE `cliente` (
+  `ID` int(11) NOT NULL,
+  `nome` varchar(20) NOT NULL,
+  `cognome` varchar(20) NOT NULL,
+  `dataNascita` date NOT NULL,
+  `email` varchar(30) NOT NULL,
+  `password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `notifica`
+--
+
+CREATE TABLE `notifica` (
+  `ID` int(11) NOT NULL,
+  `testo` varchar(100) NOT NULL,
+  `tipo` enum('spedizione','esaurimento') NOT NULL,
+  `ID_venditore` int(11) DEFAULT NULL,
+  `ID_cliente` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `ordine`
+--
+
+CREATE TABLE `ordine` (
+  `ID` int(11) NOT NULL,
+  `costoTotale` float NOT NULL,
+  `tipoPagamento` enum('contanti','carta') NOT NULL,
+  `numeroCarta` int(11) NOT NULL,
+  `dataScadenzaCarta` date NOT NULL,
+  `cvv` int(11) NOT NULL,
+  `ID_cliente` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `prodotto`
+--
+
+CREATE TABLE `prodotto` (
+  `ID_venditore` int(11) NOT NULL,
+  `nome` varchar(20) NOT NULL,
+  `immagine` blob NOT NULL,
+  `quantitàDisponibile` int(11) NOT NULL,
+  `tipoDisponibilità` enum('Immediata','5 giorni','10 giorni','1 mese') NOT NULL,
+  `prezzoUnitario` float NOT NULL,
+  `testoBreve` varchar(100) NOT NULL,
+  `testoMedio` varchar(1000) NOT NULL,
+  `testoLungo` varchar(2500) NOT NULL,
+  `dataInserimento` date NOT NULL,
+  `ID_categoria` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `prodotto_in_ordine`
+--
+
+CREATE TABLE `prodotto_in_ordine` (
+  `quantitàAcquistata` int(11) NOT NULL,
+  `nomeProdotto` varchar(20) NOT NULL,
+  `ID_ordine` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `venditore`
+--
+
+CREATE TABLE `venditore` (
+  `ID` int(11) NOT NULL,
+  `nome` varchar(20) NOT NULL,
+  `cognome` varchar(20) NOT NULL,
+  `dataNascita` date NOT NULL,
+  `email` varchar(30) NOT NULL,
+  `password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Indici per le tabelle scaricate
+--
+
+--
+-- Indici per le tabelle `categoria`
+--
+ALTER TABLE `categoria`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indici per le tabelle `cliente`
+--
+ALTER TABLE `cliente`
+  ADD PRIMARY KEY (`ID`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- Indici per le tabelle `notifica`
+--
+ALTER TABLE `notifica`
+  ADD PRIMARY KEY (`ID`),
+  ADD KEY `FKdi_venditore` (`ID_venditore`),
+  ADD KEY `FKdi_cliente` (`ID_cliente`);
+
+--
+-- Indici per le tabelle `ordine`
+--
+ALTER TABLE `ordine`
+  ADD PRIMARY KEY (`ID`),
+  ADD KEY `FKeffettua` (`ID_cliente`);
+
+--
+-- Indici per le tabelle `prodotto`
+--
+ALTER TABLE `prodotto`
+  ADD PRIMARY KEY (`nome`),
+  ADD KEY `FKinserisce` (`ID_venditore`),
+  ADD KEY `FKappartiene` (`ID_categoria`);
+
+--
+-- Indici per le tabelle `prodotto_in_ordine`
+--
+ALTER TABLE `prodotto_in_ordine`
+  ADD PRIMARY KEY (`nomeProdotto`,`ID_ordine`),
+  ADD KEY `FKR_1` (`ID_ordine`);
+
+--
+-- Indici per le tabelle `venditore`
+--
+ALTER TABLE `venditore`
+  ADD PRIMARY KEY (`ID`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- AUTO_INCREMENT per le tabelle scaricate
+--
+
+--
+-- AUTO_INCREMENT per la tabella `categoria`
+--
+ALTER TABLE `categoria`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT per la tabella `cliente`
+--
+ALTER TABLE `cliente`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT per la tabella `notifica`
+--
+ALTER TABLE `notifica`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT per la tabella `ordine`
+--
+ALTER TABLE `ordine`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT per la tabella `venditore`
+--
+ALTER TABLE `venditore`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Limiti per le tabelle scaricate
+--
+
+--
+-- Limiti per la tabella `notifica`
+--
+ALTER TABLE `notifica`
+  ADD CONSTRAINT `FKdi_cliente` FOREIGN KEY (`ID_cliente`) REFERENCES `cliente` (`ID`),
+  ADD CONSTRAINT `FKdi_venditore` FOREIGN KEY (`ID_venditore`) REFERENCES `venditore` (`ID`);
+
+--
+-- Limiti per la tabella `ordine`
+--
+ALTER TABLE `ordine`
+  ADD CONSTRAINT `FKeffettua` FOREIGN KEY (`ID_cliente`) REFERENCES `cliente` (`ID`);
+
+--
+-- Limiti per la tabella `prodotto`
+--
+ALTER TABLE `prodotto`
+  ADD CONSTRAINT `FKappartiene` FOREIGN KEY (`ID_categoria`) REFERENCES `categoria` (`ID`),
+  ADD CONSTRAINT `FKinserisce` FOREIGN KEY (`ID_venditore`) REFERENCES `venditore` (`ID`);
+
+--
+-- Limiti per la tabella `prodotto_in_ordine`
+--
+ALTER TABLE `prodotto_in_ordine`
+  ADD CONSTRAINT `FKR` FOREIGN KEY (`nomeProdotto`) REFERENCES `prodotto` (`nome`),
+  ADD CONSTRAINT `FKR_1` FOREIGN KEY (`ID_ordine`) REFERENCES `ordine` (`ID`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
