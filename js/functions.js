56628767a43f46b98105979522e8961a42dbf486
Andrea Foschi <andrea.foschi13@studio.unibo.it>
2022-02-09 00:05:50 +0100

    js and jQuery

---
+++ js/functions.js
@@ -0,0 +1,32 @@
+$(document).ready(function(){
+    let start = 0;
+    const offset = 5;
+    $(".articles article").hide();
+    showArticles(start, offset);
+
+    $("#next").click(function(){
+        start+= offset;
+        $(".articles article").hide();
+        showArticles(start, offset);
+        if($(".articles article:visible").length == 0) {
+            start-= offset;
+            showArticles(start, offset);
+        }
+    });
+
+    $("#prev").click(function(){
+        if(start - offset >= 0) {
+            start-= offset;
+            $(".articles article").hide();
+            showArticles(start, offset);
+        }
+    });
+});
+
+function showArticles(start, offset) {
+    $(".articles article").each(function(e) {
+        if (e>=start && e < start+offset) {
+            $(this).show();
+        }
+    });
+}
