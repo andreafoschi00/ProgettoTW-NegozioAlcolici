fa269f95ceb549785ec7e9cbd132247cd4526076
Atanas Todorov Atanasov <atanastodor.atanasov@studio.unibo.it>
2022-02-09 12:25:52 +0100

    add require

--- bootstrap.php
+++ bootstrap.php
@@ -1,6 +1,7 @@
 <?php
     session_start();
     define("UPLOAD_DIR", "./img/");
+    require_once("utils/functions.php");
     require_once("db/database.php");
     $dbh = new DatabaseHelper("localhost", "root", "", "negozio_alcolici_tw", 3306);
 ?>
