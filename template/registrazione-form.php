18632f5461fbad7ef0e3bc167404e2d13381eec8
Atanas Todorov Atanasov <atanastodor.atanasov@studio.unibo.it>
2022-02-18 12:31:01 +0100

    add show/hide function for password

--- template/registrazione-form.php
+++ template/registrazione-form.php
@@ -6,7 +6,7 @@
     <meta charset="UTF-8"/>
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <link rel="stylesheet" href="./css/style.css" >
+    <link rel="stylesheet" href="./css/style.css">
     <script src="./js/jquery-3.4.1.min.js" type="text/javascript"></script>
 	<script src="./js/registrazione.js" type="text/javascript"></script>
 </head>
@@ -83,7 +83,7 @@
                         <br/><br/>
                         <div class="row">
                          <div class="col-12 col-md-4">
-                            <input type="password" name="password" id="passwordReg">
+                            <input type="password" name="password" id="passInput">   
                          </div>
                          <div class="col-5 col-md-3">
                             <label>Sicurezza password:</label>
@@ -100,7 +100,7 @@
                          </div>   
                         </div>
                         <br/><br/>
-                        <label><input type="checkbox">Mostra password</label>
+                        <input type="checkbox" id="showPass"><label>Mostra password</label>
                         <br/><br/>
                         <input type="submit" name="submit" value="Registrati" class="btn btn-primary regist" id="regist">
                     </fieldset>
