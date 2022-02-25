495711fb7e33135077b518434369c5633f2e0bf3
Atanas Todorov Atanasov <atanastodor.atanasov@studio.unibo.it>
2022-02-26 00:08:59 +0100

    develop inserimento articolo

--- inserisci-articolo.php
+++ inserisci-articolo.php
@@ -2,8 +2,43 @@
 
 require_once "bootstrap.php";
 
+if($_GET["titoloarticolo"] != "" && $_GET["testoarticolo"] != "" && $_GET["imgarticolo"] != "" && $_GET["prezzoarticolo"] != ""
+   && $_GET["quantitàarticolo"] != "" && $_GET["testomedio"] != "" && $_GET["testolungo"] != ""){
+    
+    $ext = pathinfo($_GET["imgarticolo"], PATHINFO_EXTENSION);
+    if($ext != "png"){
+        $templateParams["checkImgExt"] = "Devi allegare un'immagine con estensione .png";
+    }
 
-$templateParams["categorie"] = $dbh->getCategories();
-require "template/temp-inserimento.php";
+    if(!is_numeric($_GET["prezzoarticolo"])){
+        $templateParams["checkPrezzo"] = "Devi inserire un valore numerico";
+    }
+
+    if(!is_numeric($_GET["quantitàarticolo"])){
+        $templateParams["checkQuantità"] = "Devi inserire un valore numerico";
+    }
+
+    if(!isset($templateParams["checkImgExt"]) && !isset($templateParams["checkPrezzo"]) && !isset($templateParams["checkQuantità"])){
+        $dbh->insertProduct($dbh->getIDfromMail($_SESSION["email"], $_SESSION["rank"]), $_GET["titoloarticolo"], $_GET["imgarticolo"], $_GET["quantitàarticolo"], $_GET["disponibilitàarticolo"],
+        $_GET["prezzoarticolo"], $_GET["testoarticolo"], $_GET["testomedio"], $_GET["testolungo"], date("Y-m-d"), $dbh->getIdByCategory($_GET["optradio"])[0]["ID"]);
+        
+        $templateParams["checkInserimento"] = "Inserimento eseguito con successo!";
+        $templateParams["titolo"] = "Amministrazione - Negozio Alcolici";
+        $templateParams["nome"] = "temp-amministrazione.php";
+        $templateParams["articoli"] = $dbh->getLatestProducts();
+
+        require "template/temp-amministrazione.php";
+    } else {
+        $templateParams["titolo"] = "Inserimento Articolo - Negozio Alcolici";
+        $templateParams["nome"] = "temp-inserimento.php";
+        $templateParams["categorie"] = $dbh->getCategories();
+        require "template/temp-inserimento.php";
+    }
+} else {
+    $templateParams["titolo"] = "Inserimento Articolo - Negozio Alcolici";
+    $templateParams["nome"] = "temp-inserimento.php";
+    $templateParams["categorie"] = $dbh->getCategories();
+    require "template/temp-inserimento.php";
+}
 
 ?>
--- template/temp-inserimento.php
+++ template/temp-inserimento.php
@@ -1,8 +1,8 @@
 <!DOCTYPE html>
 <html lang="it">
 <head>
-    <title>Gestione articoli - Negozio Alcolici</title>
-    <link rel="icon" type="image/x-icon" href="../img/favicon.ico">
+    <title><?php echo $templateParams["titolo"]; ?></title>
+    <link rel="icon" type="image/x-icon" href="./img/favicon.ico">
     <meta charset="UTF-8"/>
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
@@ -22,16 +22,16 @@
             <div class="col-12 col-md-10">
                 <ul class="nav nav-pills">
                     <li class="nav-item col-6 col-md-3">
-                        <a class="nav-link mt-2 mx-2 text-center text-white bg-dark" href="index.html">Home</a>
+                        <a class="nav-link mt-2 mx-2 text-center text-white bg-dark" href="index.php">Home</a>
                     </li>
                     <li class="nav-item col-6 col-md-3">
-                        <a class="nav-link mt-2 mx-2 text-center text-white bg-dark" href="catalogo.html">Catalogo</a>
+                        <a class="nav-link mt-2 mx-2 text-center text-white bg-dark" href="catalogo.php">Catalogo</a>
                     </li>
                     <li class="nav-item col-6 col-md-3">
-                        <a class="nav-link mt-2 mx-2 text-center text-white bg-dark" href="amministrazione.html">Amministrazione</a>
+                    <a class="nav-link mt-2 mx-2 text-center text-white bg-dark" href="<?php echo buttonLink();?>"><?php echo buttonText();?></a>
                     </li>
                     <li class="nav-item col-6 col-md-3">
-                        <a class="nav-link mt-2 mx-2 text-center text-white bg-dark" href="login.html">Login</a>
+                        <a class="nav-link mt-2 mx-2 text-center text-white bg-dark" href="<?php echo buttonLoginLink();?>"><?php echo buttonLoginText();?></a>
                     </li>
                 </ul> 
             </div>
@@ -44,67 +44,78 @@
             </div>
             <div class="col-md-8"></div>   
         </div>
-        <div class="row">
-            <div class="col-md-1"></div>   
-            <div class="col-12 col-md-5 mx-3 mt-3 py-1 text-dark bg-info bg-opacity-10">
-                    <ul>
-                        <li class="bottomSpacing">
-                            <label for="titoloarticolo">Titolo:</label><input type="text" id="titoloarticolo" name="titoloarticolo"/>
-                        </li>
-                        <li class="bottomSpacing">
-                            <label for="testoarticolo">Testo Breve:</label><input type="text" id="testoarticolo" name="testoarticolo"/>        
-                        </li>
-                        <li class="bottomSpacing">
-                            <label for="imgarticolo">Immagine Articolo</label><input style="" type="file" name="imgarticolo" id="imgarticolo" />   
-                            <img src="" alt=""/>
-                        </li>
-                        <li class="bottomSpacing">
-                            <label>Prezzo:</label><input type="text" name="prezzoarticolo" id="prezzoarticolo" />
-                        </li>
-                        <li class="bottomSpacing">
-                            <label>Quantità:</label><input type="text" name="quantitàarticolo" id="quantitàarticolo" />
-                        </li>
-                        <li class="bottomSpacing">
-                            <label>Disponibilità:</label>  
-                            <select class="form-select" id="sel1" name="sellist1">
-                                <option>Immediata</option>
-                                <option>5 giorni</option>
-                                <option>10 giorni</option>
-                                <option>1 mese</option>
-                            </select>
-                        </li>
-                    </ul>
+        <form action="inserisci-articolo.php" method="GET">
+            <div class="row">
+                <div class="col-md-1"></div>   
+                <div class="col-12 col-md-5 mx-3 mt-3 py-1 text-dark bg-info bg-opacity-10">
+                        <ul>
+                            <li class="bottomSpacing">
+                                <label class="fontBolder" for="titoloarticolo">Titolo:</label><input type="text" id="titoloarticolo" name="titoloarticolo"/>
+                            </li>
+                            <li class="bottomSpacing">
+                                <label class="fontBolder" for="testoarticolo">Testo Breve:</label><input type="text" id="testoarticolo" name="testoarticolo"/>        
+                            </li>
+                            <li class="bottomSpacing">
+                                <label class="fontBolder" for="imgarticolo">Immagine Articolo</label><input style="" type="file" name="imgarticolo" id="imgarticolo" />   
+                                <img src="" alt=""/>
+                            </li>
+                            <?php if(isset($templateParams["checkImgExt"])): ?>    
+                            <li class ="bottomSpacing"><label><?php echo $templateParams["checkImgExt"]; ?></label></li>
+                            <?php endif; ?>
+                            <li class="bottomSpacing">
+                                <label class="fontBolder">Prezzo:</label><input type="text" name="prezzoarticolo" id="prezzoarticolo" />
+                            </li>
+                            <?php if(isset($templateParams["checkPrezzo"])): ?>    
+                            <li class ="bottomSpacing"><label><?php echo $templateParams["checkPrezzo"]; ?></label></li>
+                            <?php endif; ?>
+                            <li class="bottomSpacing">
+                                <label class="fontBolder">Quantità:</label><input type="text" name="quantitàarticolo" id="quantitàarticolo" />
+                            </li>
+                            <?php if(isset($templateParams["checkQuantità"])): ?>    
+                            <li class ="bottomSpacing"><label><?php echo $templateParams["checkQuantità"]; ?></label></li>
+                            <?php endif; ?>
+                            <li class="bottomSpacing">
+                                <label class="fontBolder">Disponibilità:</label>  
+                                <select class="form-select" id="sel1" name="disponibilitàarticolo">
+                                    <option>Immediata</option>
+                                    <option>5 giorni</option>
+                                    <option>10 giorni</option>
+                                    <option>1 mese</option>
+                                </select>
+                            </li>
+                        </ul>
+                </div>
+                <div class="col-12 col-md-5 mt-3 py-1 text-dark bg-info bg-opacity-10">
+                        <ul>
+                            <li>
+                                <div class="row"><label class="fontBolder" for="titoloarticolo">Testo medio:</label></div>
+                                <div class="row"><textarea id="testomedio" name="testomedio"></textarea></div>
+                            </li>
+                            <li>
+                                <div class="row"><label class="fontBolder" for="testoarticolo">Testo lungo:</label></div>
+                                <div class="row"><textarea id="testolungo" name="testolungo"></textarea></div>        
+                            </li>
+                            <li>
+                                <label class="fontBolder">Categoria:</label>
+                                <?php foreach($templateParams["categorie"] as $categorie): ?>
+                                <div class="form-check">
+                                    <input type="radio" class="form-check-input" id="radio1" name="optradio" value="<?php echo $categorie["nome"]; ?>" checked>
+                                    <label class="form-check-label" for="radio1"><?php echo $categorie["nome"]; ?></label>
+                                </div>
+                                <?php endforeach; ?>
+                            </li>
+                        </ul>
+                </div>
+                <div class="col-md-1"></div>
             </div>
-            <div class="col-12 col-md-5 mt-3 py-1 text-dark bg-info bg-opacity-10">
-                    <ul>
-                        <li>
-                            <div class="row"><label for="titoloarticolo">Testo medio:</label></div>
-                            <div class="row"><textarea id="testomedio" name="testomedio"></textarea></div>
-                        </li>
-                        <li>
-                            <div class="row"><label for="testoarticolo">Testo lungo:</label></div>
-                            <div class="row"><textarea id="testolungo" name="testolungo"></textarea></div>        
-                        </li>
-                        <li>
-                            <label>Categoria:</label>
-                            <?php foreach($templateParams["categorie"] as $categorie): ?>
-                            <div class="form-check">
-                                <input type="radio" class="form-check-input" id="radio1" name="optradio" value="option1" checked>
-                                <label class="form-check-label" for="radio1"><?php echo $categorie["nome"]; ?></label>
-                            </div>
-                            <?php endforeach; ?>
-                        </li>
-                    </ul>
-            </div>
-            <div class="col-md-1"></div>
-        </div>
         <div class="row">
             <div class="col-md-1"></div>
             <div class="col-12 col-md-10 mx-3 px-2 mt-3 py-1 text-dark bg-info bg-opacity-10">
-                <input type="submit" value="Inserisci" class="btn btn-primary"/>
+                <input type="submit" name="submit" value="Inserisci" class="btn btn-primary"/>
             </div>
             <div class="col-md-1"></div>
         </div>
+        </form>
         <div class="row">
             <div class="col-12">
                     <footer class="mt-4 py-1 text-dark bg-info bg-opacity-25">
