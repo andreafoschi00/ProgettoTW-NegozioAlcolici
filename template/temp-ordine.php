7de9434dd680f05cd450b1dacff363f80d95018f
Atanas Todorov Atanasov <atanastodor.atanasov@studio.unibo.it>
2022-02-28 21:31:15 +0100

    rename favicon href

--- template/temp-ordine.php
+++ template/temp-ordine.php
@@ -2,7 +2,7 @@
 <html lang="it">
 <head>
     <title><?php echo $templateParams["titolo"]; ?></title>
-    <link rel="icon" type="image/x-icon" href="./img/favicon.ico">
+    <link rel="icon" type="image/x-icon" href=<?php echo UPLOAD_DIR."favicon.ico"?>>
     <meta charset="utf-8"/>
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
