d16b469506aedf55c24af625106d0527324ae96a
Andrea Foschi <andrea.foschi13@studio.unibo.it>
2022-02-28 19:08:37 +0100

    adjust order by

--- db/database.php
+++ db/database.php
@@ -422,7 +422,7 @@
             $stmt = $this->db->prepare("SELECT prodotto.ID as IDprodotto, prodotto.nome as nomeProdotto, nomeImmagine, testoMedio, dataInserimento, venditore.nome as nomeVenditore, venditore.cognome as cognomeVenditore
                                         FROM prodotto, venditore
                                         WHERE prodotto.ID_venditore = ? AND venditore.ID = prodotto.ID_venditore
-                                        ORDER BY prodotto.dataInserimento DESC");
+                                        ORDER BY prodotto.dataInserimento DESC, IDprodotto DESC");
             $stmt->bind_param('i', $id);
             $stmt->execute();
             $result = $stmt->get_result();
