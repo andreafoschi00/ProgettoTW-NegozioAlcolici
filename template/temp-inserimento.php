4bb6b1e23f81d0ffa3b69074eb906abdb94523f4
Atanas Todorov Atanasov <atanastodor.atanasov@studio.unibo.it>
2022-05-30 12:20:45 +0200

    fix accessability

--- template/temp-inserimento.php
+++ template/temp-inserimento.php
@@ -46,75 +46,78 @@
         </div>
         <form action="<?php if(isset($templateParams["prodotto"])) echo "modifica-articolo.php"; 
         else echo "inserisci-articolo.php"; ?>" method="GET">
-            <div class="row">
-                <div class="col-md-1"></div>   
-                <div class="col-12 col-md-5 mx-3 mt-3 py-1 text-dark bg-info bg-opacity-10">
-                        <ul>
-                            <li class="bottomSpacing">
-                                <label class="fontBolder" for="titoloarticolo">Titolo:</label><input type="text" value="<?php if(isset($templateParams["prodotto"])) echo $templateParams["prodotto"][0]["nomeProdotto"]; ?>" id="titoloarticolo" name="titoloarticolo" required class="removeHover"/>
-                            </li>
-                            <li class="bottomSpacing">
-                                <label class="fontBolder" for="testoarticolo">Testo Breve:</label><input type="text" value="<?php if(isset($templateParams["prodotto"])) echo $templateParams["prodotto"][0]["testoBreve"]; ?>" id="testoarticolo" name="testoarticolo" required class="removeHover"/>        
-                            </li>
-                            <li class="bottomSpacing">
-                                <label class="fontBolder" for="imgarticolo">Immagine Articolo</label><input type="file" name="imgarticolo" id="imgarticolo" required class="removeHover"/>   
-                            </li>
-                            <?php if(isset($templateParams["checkImgExt"])): ?>    
-                            <li class ="bottomSpacing"><p class="msgError"><?php echo $templateParams["checkImgExt"]; ?></p></li>
-                            <?php endif; ?>
-                            <li class="bottomSpacing">
-                                <label class="fontBolder">Prezzo:</label><input type="text" value="<?php if(isset($templateParams["prodotto"])) echo $templateParams["prodotto"][0]["prezzoUnitario"]; ?>" name="prezzoarticolo" id="prezzoarticolo" required class="removeHover"/>
-                            </li>
-                            <?php if(isset($templateParams["checkPrezzo"])): ?>    
-                            <li class ="bottomSpacing"><p class="msgError"><?php echo $templateParams["checkPrezzo"]; ?></p></li>
-                            <?php endif; ?>
-                            <li class="bottomSpacing">
-                                <label class="fontBolder">Quantit&agrave;:</label><input type="text" value="<?php if(isset($templateParams["prodotto"])) echo $templateParams["prodotto"][0]["quantitàDisponibile"]; ?>" name="quantit&agrave;articolo" id="quantit&agrave;articolo" required class="removeHover"/>
-                            </li>
-                            <?php if(isset($templateParams["checkQuantità"])): ?>    
-                            <li class ="bottomSpacing"><p class="msgError"><?php echo $templateParams["checkQuantità"]; ?></p></li>
-                            <?php endif; ?>
-                            <li class="bottomSpacing">
-                                <label class="fontBolder">Disponibilit&agrave;:</label>  
-                                <select class="form-select" id="sel1" name="disponibilit&agrave;articolo" required size="4">
-                                    <option <?php if(isset($templateParams["prodotto"]) && $templateParams["prodotto"][0]["tipoDisponibilità"] == "Immediata")  echo "selected=selected"; ?>>Immediata</option>
-                                    <option <?php if(isset($templateParams["prodotto"]) && $templateParams["prodotto"][0]["tipoDisponibilità"] == "5 giorni")  echo "selected=selected"; ?>>5 giorni</option>
-                                    <option <?php if(isset($templateParams["prodotto"]) && $templateParams["prodotto"][0]["tipoDisponibilità"] == "10 giorni")  echo "selected=selected"; ?>>10 giorni</option>
-                                    <option <?php if(isset($templateParams["prodotto"]) && $templateParams["prodotto"][0]["tipoDisponibilità"] == "1 mese")  echo "selected=selected"; ?>>1 mese</option>
-                                </select>
-                            </li>
-                        </ul>
+        <fieldset>
+            <legend>Campi</legend>
+                <div class="row">
+                    <div class="col-md-1"></div>   
+                    <div class="col-12 col-md-5 mx-3 mt-3 py-1 text-dark bg-info bg-opacity-10">
+                            <ul>
+                                <li class="bottomSpacing">
+                                    <label class="fontBolder" for="titoloarticolo">Titolo:</label><input type="text" value="<?php if(isset($templateParams["prodotto"])) echo $templateParams["prodotto"][0]["nomeProdotto"]; ?>" id="titoloarticolo" name="titoloarticolo" required class="removeHover"/>
+                                </li>
+                                <li class="bottomSpacing">
+                                    <label class="fontBolder" for="testoarticolo">Testo Breve:</label><input type="text" value="<?php if(isset($templateParams["prodotto"])) echo $templateParams["prodotto"][0]["testoBreve"]; ?>" id="testoarticolo" name="testoarticolo" required class="removeHover"/>        
+                                </li>
+                                <li class="bottomSpacing">
+                                    <label class="fontBolder" for="imgarticolo">Immagine Articolo</label><input type="file" name="imgarticolo" id="imgarticolo" required class="removeHover"/>   
+                                </li>
+                                <?php if(isset($templateParams["checkImgExt"])): ?>    
+                                <li class ="bottomSpacing"><p class="msgError"><?php echo $templateParams["checkImgExt"]; ?></p></li>
+                                <?php endif; ?>
+                                <li class="bottomSpacing">
+                                    <label class="fontBolder" for="prezzoarticolo">Prezzo:</label><input type="text" value="<?php if(isset($templateParams["prodotto"])) echo $templateParams["prodotto"][0]["prezzoUnitario"]; ?>" name="prezzoarticolo" id="prezzoarticolo" required class="removeHover"/>
+                                </li>
+                                <?php if(isset($templateParams["checkPrezzo"])): ?>    
+                                <li class ="bottomSpacing"><p class="msgError"><?php echo $templateParams["checkPrezzo"]; ?></p></li>
+                                <?php endif; ?>
+                                <li class="bottomSpacing">
+                                    <label class="fontBolder" for="quantit&agrave;articolo">Quantit&agrave;:</label><input type="text" value="<?php if(isset($templateParams["prodotto"])) echo $templateParams["prodotto"][0]["quantitàDisponibile"]; ?>" name="quantit&agrave;articolo" id="quantit&agrave;articolo" required class="removeHover"/>
+                                </li>
+                                <?php if(isset($templateParams["checkQuantità"])): ?>    
+                                <li class ="bottomSpacing"><p class="msgError"><?php echo $templateParams["checkQuantità"]; ?></p></li>
+                                <?php endif; ?>
+                                <li class="bottomSpacing">
+                                    <label class="fontBolder" for="sel1">Disponibilit&agrave;:</label>  
+                                    <select class="form-select" id="sel1" name="disponibilit&agrave;articolo" required size="4">
+                                        <option id="immediata" <?php if(isset($templateParams["prodotto"]) && $templateParams["prodotto"][0]["tipoDisponibilità"] == "Immediata")  echo "selected=selected"; ?>><label for="immediata">Immediata</label></option>
+                                        <option id="5giorni" <?php if(isset($templateParams["prodotto"]) && $templateParams["prodotto"][0]["tipoDisponibilità"] == "5 giorni")  echo "selected=selected"; ?>><label for="5giorni">5 giorni</label></option>
+                                        <option id="10giorni" <?php if(isset($templateParams["prodotto"]) && $templateParams["prodotto"][0]["tipoDisponibilità"] == "10 giorni")  echo "selected=selected"; ?>><label for="10giorni">10 giorni</label></option>
+                                        <option id="1mese" <?php if(isset($templateParams["prodotto"]) && $templateParams["prodotto"][0]["tipoDisponibilità"] == "1 mese")  echo "selected=selected"; ?>><label for="1mese">1 mese</label></option>
+                                    </select>
+                                </li>
+                            </ul>
+                    </div>
+                    <div class="col-12 col-md-5 mt-3 py-1 text-dark bg-info bg-opacity-10">
+                            <ul>
+                                <li>
+                                    <div class="row"><label class="fontBolder" for="testomedio">Testo medio:</label></div>
+                                    <div class="row"><textarea id="testomedio" name="testomedio" required><?php if(isset($templateParams["prodotto"])) echo $templateParams["prodotto"][0]["testoMedio"]; ?></textarea></div>
+                                </li>
+                                <li>
+                                    <div class="row"><label class="fontBolder" for="testolungo">Testo lungo:</label></div>
+                                    <div class="row"><textarea id="testolungo"  name="testolungo" required><?php if(isset($templateParams["prodotto"])) echo $templateParams["prodotto"][0]["testoLungo"]; ?></textarea></div>        
+                                </li>
+                                <li>
+                                    <label class="fontBolder">Categoria:</label>
+                                    <?php foreach($templateParams["categorie"] as $categorie): ?>
+                                    <div class="form-check">
+                                        <input type="radio" id="<?php echo $categorie["nome"];?>" <?php if(isset($templateParams["prodotto"]) && $templateParams["prodotto"][0]["nomeCategoria"] == $categorie["nome"])  echo "checked"; ?> class="form-check-input" name="optradio" value="<?php echo $categorie["nome"]; ?>"  required>
+                                        <label class="form-check-label" for="<?php echo $categorie["nome"]; ?>"><?php echo $categorie["nome"]; ?></label>
+                                    </div>
+                                    <?php endforeach; ?>
+                                </li>
+                            </ul>
+                    </div>
+                    <div class="col-md-1"></div>
                 </div>
-                <div class="col-12 col-md-5 mt-3 py-1 text-dark bg-info bg-opacity-10">
-                        <ul>
-                            <li>
-                                <div class="row"><label class="fontBolder" for="titoloarticolo">Testo medio:</label></div>
-                                <div class="row"><textarea id="testomedio" name="testomedio" required><?php if(isset($templateParams["prodotto"])) echo $templateParams["prodotto"][0]["testoMedio"]; ?></textarea></div>
-                            </li>
-                            <li>
-                                <div class="row"><label class="fontBolder" for="testoarticolo">Testo lungo:</label></div>
-                                <div class="row"><textarea id="testolungo"  name="testolungo" required><?php if(isset($templateParams["prodotto"])) echo $templateParams["prodotto"][0]["testoLungo"]; ?></textarea></div>        
-                            </li>
-                            <li>
-                                <label class="fontBolder">Categoria:</label>
-                                <?php foreach($templateParams["categorie"] as $categorie): ?>
-                                <div class="form-check">
-                                    <input type="radio" <?php if(isset($templateParams["prodotto"]) && $templateParams["prodotto"][0]["nomeCategoria"] == $categorie["nome"])  echo "checked"; ?> class="form-check-input" name="optradio" value="<?php echo $categorie["nome"]; ?>"  required>
-                                    <label class="form-check-label"><?php echo $categorie["nome"]; ?></label>
-                                </div>
-                                <?php endforeach; ?>
-                            </li>
-                        </ul>
+            <div class="row">
+                <div class="col-md-1"></div>
+                <div class="col-12 col-md-10 mx-3 px-2 mt-3 py-1 text-dark bg-info bg-opacity-10">
+                    <input type="submit" name="submit" value="<?php echo $_SESSION["azione"]; ?>" class="btn btn-primary" />
                 </div>
                 <div class="col-md-1"></div>
             </div>
-        <div class="row">
-            <div class="col-md-1"></div>
-            <div class="col-12 col-md-10 mx-3 px-2 mt-3 py-1 text-dark bg-info bg-opacity-10">
-                <input type="submit" name="submit" value="<?php echo $_SESSION["azione"]; ?>" class="btn btn-primary" />
-            </div>
-            <div class="col-md-1"></div>
-        </div>
+        </fieldset>
         </form>
         <div class="row">
             <div class="col-12">
